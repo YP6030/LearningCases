ec75124b178847107ba7bf52fe78c0afe67023bf
YP6030 <1052314265@qq.com>
2023-09-03 16:08:30 +0800

    新增函数和枚举案例

--- 7.函数/Program.cs
+++ 7.函数/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        static int InputNum(int a)
+        {
+            int num;
+            Console.WriteLine($"请输入第{a}个数字:");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------");
+            return num;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a, b, c;
+            a = InputNum(1);
+            b = InputNum(2);
+            c = InputNum(3);
+            Console.WriteLine($"{a} {b} {c}，这三个数字的和为：" + (a + b + c));
         }
     }
 }
---
+++ 8.enum枚举/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _8.enum枚举
+{
+    internal class Program
+    {
+        enum 星期
+        {
+            星期一 = 1,
+            星期二,
+            星期三,
+            星期四,
+            星期五,
+            星期六,
+            星期日,
+        }
+        enum 水果
+        {
+            苹果 = 1,
+            香蕉,
+            桔子,
+            葡萄,
+        }
+        enum Fruit
+        {
+            Apple = 1,
+            Banana,
+            Orange,
+            Grape,
+        }
+        enum Vaule
+        {
+            Apple = 4,
+            Banana = 6,
+            Orange = 5,
+            Grape = 20,
+        }
+        static void 计算价格()
+        {
+            水果 tem;
+            Fruit fruit;
+            int weight = 0, menoy = 0, value = 0;
+            Console.WriteLine("请输入需要购买的水果");
+            tem = (水果)int.Parse(Console.ReadLine());
+            fruit = (Fruit)((int)tem);
+            switch((int)tem)
+            {
+                case 1:
+                    menoy = (int)Vaule.Apple;
+                    break;
+                case 2:
+                    menoy = (int)Vaule.Banana;
+                    break;
+                case 3:
+                    menoy = (int)Vaule.Orange;
+                    break;
+                case 4:
+                    menoy = (int)Vaule.Grape;
+                    break;
+            }
+            Console.WriteLine("请输入需要购买几斤");
+            weight = int.Parse(Console.ReadLine());
+            value = weight * menoy;
+
+
+            Console.WriteLine($"您购买了{weight}斤{tem},英文为{fruit}，单价为{menoy}元每斤，总价格为{value}元，请问扫码还是现金支付？");
+        }
+        static void Main(string[] args)
+        {
+            星期 day = 星期.星期三;
+            Console.WriteLine(day);
+            Console.WriteLine((int)day);
+            Console.WriteLine((星期)3);
+
+            计算价格();
+        }
+    }
+}
