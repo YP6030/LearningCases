c186dbfeb6fca27c88535d39e4f04af983698c00
YP6030 <1052314265@qq.com>
2023-09-07 16:28:55 +0800

    新增例程

--- 11.类和对象/Program.cs
+++ 11.类和对象/Program.cs
@@ -35,15 +35,61 @@
             public string name;
             public int hp;
             public int attack;
-            int def;
+            public int def;
+        }
+
+        static void PrintPlayer(Cha player)
+        {
+            Console.WriteLine(player.name);
+            Console.WriteLine(player.hp);
+            Console.WriteLine(player.attack);
+            Console.WriteLine(player.def);
         }
 
         static void Main(string[] args)
         {
             Cha player1 = new Cha();
             Cha player2 = new Cha();
+            int damage = 0;
 
             player1.name = "皮卡丘";
+            player1.hp = 30;
+            player1.attack = 20;
+            player1.def = 0;
+
+            player2.name = "妙蛙种子";
+            player2.hp = 20;
+            player2.attack = 15;
+            player2.def = 5;
+
+            PrintPlayer(player1);
+            PrintPlayer(player2);
+
+            while (true)
+            {
+                Console.WriteLine($"----------{player1.name} 进攻-----------");
+                damage = player1.attack - player2.def;
+                player2.hp -= damage;
+                Console.WriteLine($"{player1.name}造成{damage}伤害");
+                PrintPlayer(player1);
+                PrintPlayer(player2);
+                if (player2.hp <= 0)
+                {
+                    Console.WriteLine($"{player1.name} 获胜");
+                    break;
+                }
+                Console.WriteLine($"----------{player2.name} 进攻-----------");
+                damage = player2.attack - player1.def;
+                player1.hp -= damage;
+                Console.WriteLine($"{player2.name}造成{damage}伤害");
+                PrintPlayer(player1);
+                PrintPlayer(player2);
+                if (player1.hp <= 0)
+                {
+                    Console.WriteLine($"{player2.name} 获胜");
+                    break;
+                }
+            }
         }
     }
 }
---
+++ 12.构造方法，对象方法/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Numerics;
+
+namespace _12.构造方法_对象方法
+{
+    
+    internal class Program
+    {
+        static Random random = new Random();
+        class Cha
+        {
+            //字段
+            public string name;
+            public int hp;
+            public int attack;
+            public int def;
+
+            //构造函数
+            //1.无参数
+            public Cha()
+            {
+                this.name = "Test";
+            }
+            //2.用多个参数创建对象
+            public Cha(string name, int hp, int attack, int def)
+            {
+                this.name = name;
+                this.hp = hp;
+                this.attack = attack;
+                this.def = def;
+            }
+            //3.用2个参数创建对象，其它字段位默认值
+            public Cha(string name, int hp)
+            {
+                this.name = name;
+                this.hp = hp;
+                this.attack = 1;
+                this.def = 1;
+            }
+            public Cha(string name)
+            {
+                this.name = name;
+                this.hp = random.Next(50, 101);
+                this.attack = random.Next(10, 40);
+                this.def = random.Next(0, 10);
+            }
+
+            //方法
+            public void Print()
+            {
+                Console.WriteLine(this.name);
+                Console.WriteLine(this.hp);
+                Console.WriteLine(this.attack);
+                Console.WriteLine(this.def);
+            }
+            //攻击一个玩家
+            public int Attack(Cha other)
+            {
+                Console.WriteLine($"------{this.name} 进攻-------{other.name}-----------");
+                int damage = this.attack - other.def;
+                if (damage > 0)
+                {
+                    other.hp -= damage;
+                    if (other.hp < 0) other.hp = 0;
+                }
+                Console.WriteLine($"对 {other.name} 造成 {damage} 伤害");
+                return damage;
+            }
+            //自己是否死亡
+            public bool IsDie()
+            {
+                if(this.hp <= 0)
+                    return true;
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cha player1 = new Cha("皮卡丘");
+            Cha player2 = new Cha("杰尼龟");
+
+            player1.Print();
+            player2.Print();
+            while(true)
+            {
+                player1.Attack(player2);
+                player1.Print();
+                player2.Print();
+                if (player2.IsDie())
+                {
+                    Console.WriteLine($"{player1.name} 获胜");
+                    break;
+                }
+                player2.Attack(player1);
+                player1.Print();
+                player2.Print();
+                if (player1.IsDie())
+                {
+                    Console.WriteLine($"{player2.name} 获胜");
+                    break;
+                }
+            }
+
+        }
+    }
+}
