f98c69dfbb9ce5e5179a0179a1de34e6275f635d
YP6030 <1052314265@qq.com>
2023-09-08 10:34:18 +0800

    新增例程

---
+++ 13.引用类型和值类型/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _13.引用类型和值类型
+{
+    internal class Program
+    {
+        //创建类
+        class Students
+        {
+            public string name;
+            public int age;
+            public int score;
+
+            public Students(string name, int age, int score)
+            {
+                this.name = name;
+                this.age = age;
+                this.score = score;
+            }
+
+            public void PrintScore()
+            {
+                Console.WriteLine($"{this.name}的成绩为{this.score}");
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{this.name}");
+                Console.WriteLine($"{this.age}");
+                Console.WriteLine($"{this.score}");
+            }
+
+            public Students Copy()
+            {
+                Students other = new Students(this.name, this.age, this.score);
+                return other;
+            }
+        }
+
+        static void AddScore(Students other)
+        {
+            if (other != null)
+            {
+                other.score += 10;
+            }
+        }
+
+        static void func(int[] array)
+        {
+            array[0] = 888;
+            array = new int[3];
+            array[0] = 999;
+        }
+
+        static void Main(string[] args)
+        {
+            //引用类型
+            Students student1 = new Students("小明", 15, 50);
+            //AddScore(student1);
+            //student1.PrintScore();
+            //如果是值类型则必须要返回赋值才能改变
+
+
+            Students student2 = student1.Copy();
+
+            AddScore(student2);
+
+            student1.Print();
+            student2.Print();
+
+            //int[] array = new int[] { 1, 2, 3 };
+            //func(array);
+            //Console.WriteLine(array[0]);
+        }
+    }
+}
---
+++ 14.静态和属性/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace _14.静态和属性
+{
+    internal class Program
+    {
+        //class Cha
+        //{
+        //    static public int count;
+        //    public string name;
+        //    public int hp;
+        //    public int attack;
+        //    public int def;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    //Cha player1 = new Cha();
+        //    //Console.WriteLine(Cha.count);
+        //    ////访问static关键字修饰的字段
+        //    //Cha.count++;
+
+        //}
+
+        static Random random = new Random();
+        class Cha
+        {
+            static public int count;
+            //字段
+            public string name { get; private set; }
+            public int hp { get; private set; }
+            public int attack { get; private set; }
+            public int def { get; private set; }
+
+            //构造函数
+            public Cha(string name)
+            {
+                this.name = name;
+                this.hp = random.Next(50, 101);
+                this.attack = random.Next(10, 40);
+                this.def = random.Next(0, 10);
+                count++;
+            }
+
+            //方法
+            public void Print()
+            {
+                Console.WriteLine(this.name);
+                Console.WriteLine(this.hp);
+                Console.WriteLine(this.attack);
+                Console.WriteLine(this.def);
+            }
+            //攻击一个玩家
+            public int Attack(Cha other)
+            {
+                Console.WriteLine($"------{this.name} 进攻-------{other.name}-----------");
+                int damage = this.attack - other.def;
+                if (damage > 0)
+                {
+                    other.hp -= damage;
+                    if (other.hp < 0) other.hp = 0;
+                }
+                Console.WriteLine($"对 {other.name} 造成 {damage} 伤害");
+                return damage;
+            }
+            //自己是否死亡
+            public bool IsDie()
+            {
+                if (this.hp <= 0)
+                    return true;
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cha player1 = new Cha("皮卡丘");
+            Cha player2 = new Cha("杰尼龟");
+
+            Console.WriteLine($"总共有{Cha.count}个玩家");
+            player1.Print();
+            player2.Print();
+            while (true)
+            {
+                player1.Attack(player2);
+                player1.Print();
+                player2.Print();
+                if (player2.IsDie())
+                {
+                    Console.WriteLine($"{player1.name} 获胜");
+                    break;
+                }
+                player2.Attack(player1);
+                player1.Print();
+                player2.Print();
+                if (player1.IsDie())
+                {
+                    Console.WriteLine($"{player2.name} 获胜");
+                    break;
+                }
+            }
+
+        }
+    }
+}
