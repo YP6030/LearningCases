87f910c121b41e568d17592789ecb3d96881073c
YP6030 <1052314265@qq.com>
2023-09-20 16:46:50 +0800

    新增例程

---
+++ 19.Path类/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _19.Path类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"C:\3000soft\Red Spider\Data\Message\张三.wav";
+
+            //获取文件名
+            Console.WriteLine(Path.GetFileName(str));
+            //获取文件名不含扩展名
+            Console.WriteLine(Path.GetFileNameWithoutExtension(str));
+            //获取文件的扩展名
+            Console.WriteLine(Path.GetExtension(str));
+            //获取文件所在的文件夹的名称
+            Console.WriteLine(Path.GetDirectoryName(str));
+            //获取文件所在的全路径
+            Console.WriteLine(Path.GetFullPath(str));
+            //链接两个字符串作为路径
+            Console.WriteLine(Path.Combine(@"c:\a\", "b.txt"));
+        }
+    }
+}
---
+++ 20.File类/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System;
+
+namespace _20.File类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Desktop\学习资料\C#\test\test.txt";
+            //创建一个文件
+            //File.Create(@"D:\Desktop\学习资料\C#\test\new.csv");
+
+            //删除一个文件
+            //File.Delete(@"D:\Desktop\学习资料\C#\test\new.txt");
+
+            //复制一个文件
+            //File.Copy(@"D:\Desktop\学习资料\C#\test\test.txt", @"D:\Desktop\学习资料\C#\test\new1.txt");
+
+            //剪贴一个文件
+            //File.Move(@"D:\Desktop\学习资料\C#\test\test.txt", @"D:\Desktop\学习资料\C#\test.txt");
+
+            //以二进制读取整个文件
+            //byte[] buff = File.ReadAllBytes(path);
+            //string str = Encoding.Default.GetString(buff);
+            //Console.WriteLine(str);
+
+            //按行读取这个文件
+            //string[] buff = File.ReadAllLines(path);
+            //foreach(var i in buff)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //读取整个文件
+            //string buff = File.ReadAllText(path);
+            //Console.WriteLine(buff);
+
+
+            //给文件以二进制写入所有数据，覆盖
+            //string dataStr = "写入hello world";
+            //byte[] buff = Encoding.UTF8.GetBytes(dataStr);
+            //File.WriteAllBytes(path, buff);//WriteAllBytes没有这个文件的时候，会自动创建。有的话会覆盖内容
+
+            //以行的方式写入文件
+            //string[] strings = { "大家好", "好什么" };
+            //File.WriteAllLines(@"D:\Desktop\学习资料\C#\test\test1.txt", strings);
+
+            //以字符串的形式写入文件
+            //string str = "大家好才是真的好";
+            //File.WriteAllText(@"D:\Desktop\学习资料\C#\test\test1.txt", str);
+
+            //以行的方式追加写入文件
+            //string[] strings = { "大家好", "好什么" };
+            //File.AppendAllLines(path, strings);
+
+            //以字符串的形式追加写入文件
+            //string str = "大家好才是真的好";
+            //File.AppendAllText(path, str);
+        }
+    }
+}
