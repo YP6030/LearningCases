99d2ad000aa201a8bd3abe31997a2a5099086962
YP6030 <1052314265@qq.com>
2023-09-21 13:27:05 +0800

    新增例程

---
+++ 21.FileStream类/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _21.FileStream类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\..\..\..\test\test.txt";
+
+            //读取文件
+            using (FileStream fsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int len = 0;
+                string str;
+                len = fsRead.Read(buffer, 0, buffer.Length);
+                str = Encoding.UTF8.GetString(buffer, 0, len);
+                Console.WriteLine(str);
+            }
+
+            //给文件追加数据
+            using (FileStream fsWrite = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                string str = "FileStream Write";
+                byte[] buffer = Encoding.UTF8.GetBytes(str);
+                fsWrite.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
---
+++ 22.StreamReader和StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _22.StreamReader和StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\..\..\..\test\test.txt";
+
+            using (StreamReader stRead = new StreamReader(path, Encoding.UTF8))
+            {
+                string? str;
+                while (!stRead.EndOfStream)
+                {
+                    str = stRead.ReadLine();
+                    Console.WriteLine(str);
+                }
+            }
+
+            using (StreamWriter stWrite = new StreamWriter(path, true, Encoding.UTF8))
+            {
+                string str = "StreamWriter 追加写入";
+                stWrite.Write(str);//写完后没有换行
+                stWrite.WriteLine(str);//写完后有换行
+            }
+        }
+    }
+}
